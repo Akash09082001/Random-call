18868c9b094f02af54c4dcb032d89ebe42d885cb
Akash09082001 <aakashl@letsupgrade.in>
2024-03-18 18:31:22 +0530

    add.

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-function makeCall() {
+
+// form modal
+const callPopupModal = document.getElementById("callPopup");
+const callNowButton = document.getElementById("callNowButton");
+
+function callPopup() {
+    callPopupModal.style.display = "flex";
+
+    setTimeout(() => {
+        callPopupModal.style.display = 'none';
+    }, 4000);
+
+}
+
+callNowButton.addEventListener('click', function makeCall() {
     var currentTime = new Date();
     var currentHour = currentTime.getHours();
 
     if (currentHour >= 9 && currentHour < 19) {
-        var mobileNumbers = ['8898814214', '8169444762', '9820160368', '9920907303'];
+        var mobileNumbers = ['8976778199', '8657536255', '8657760471', '8657547924', '8657760480'];
 
         var randomIndex = Math.floor(Math.random() * mobileNumbers.length);
         var selectedNumber = mobileNumbers[randomIndex];
 
-        let call = document.getElementById("callNowButton").href = "tel:" + selectedNumber;
+        let call = document.getElementById("callNowButton").href = "tel:+91 " + selectedNumber;
         return call
     } else {
         document.getElementById("callNowButton").removeAttribute("href");
-        alert("Sorry, the call service is only available between 9am and 7pm.");
+        callPopup()
     }
-}
+})
+
