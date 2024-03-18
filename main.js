0aaf3cb30f7eb2c9d910ddc06696728290e33b87
Akash09082001 <aakashl@letsupgrade.in>
2024-03-18 12:54:19 +0530

    add.

--- main.js
+++ main.js
@@ -2,19 +2,15 @@ function makeCall() {
     var currentTime = new Date();
     var currentHour = currentTime.getHours();
 
-    // Check if the current time is between 9am and 7pm
     if (currentHour >= 9 && currentHour < 19) {
         var mobileNumbers = ['8898814214', '8169444762', '9820160368', '9920907303'];
 
-        // Choose a random mobile number from the array
         var randomIndex = Math.floor(Math.random() * mobileNumbers.length);
         var selectedNumber = mobileNumbers[randomIndex];
 
-        // Call the function to make the call using the selected number
-        const call = document.getElementById("callNowButton").href = "tel:" + selectedNumber;
-        console.log(call);
+        let call = document.getElementById("callNowButton").href = "tel:" + selectedNumber;
+        return call
     } else {
-        // If the current time is outside the allowed hours, disable the button
         document.getElementById("callNowButton").removeAttribute("href");
         alert("Sorry, the call service is only available between 9am and 7pm.");
     }
